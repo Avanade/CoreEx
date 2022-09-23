aadaa4862763f09cca507a9c9f8e947be87c68d0
Piotr <karpik.pl+nospam@gmail.com>
2022-09-23 18:32:03 -0400

    renaming database
    Signed-off-by: Piotr <karpik.pl+nospam@gmail.com>

--- src/Templates/content/Company.AppName.Business/Data/AppNameEfDb.cs
+++ src/Templates/content/Company.AppName.Business/Data/AppNameEfDb.cs
@@ -5,7 +5,7 @@ namespace Company.AppName.Business.Data
     /// <summary>
     /// Enables the <b>Company.AppName</b> database using Entity Framework.
     /// </summary>
-    public interface IHrEfDb : IEfDb
+    public interface IAppNameEfDb : IEfDb
     {
         /// <summary>
         /// Gets the <see cref="Employee"/> entity.
@@ -16,14 +16,14 @@ namespace Company.AppName.Business.Data
     /// <summary>
     /// Represents the <b>Company.AppName</b> database using Entity Framework.
     /// </summary>
-    public class HrEfDb : EfDb<AppNameDbContext>, IHrEfDb
+    public class AppNameEfDb : EfDb<AppNameDbContext>, IAppNameEfDb
     {
         /// <summary>
-        /// Initializes a new instance of the <see cref="HrEfDb"/> class.
+        /// Initializes a new instance of the <see cref="AppNameEfDb"/> class.
         /// </summary>
         /// <param name="dbContext">The entity framework database context.</param>
         /// <param name="mapper">The <see cref="IMapper"/>.</param>
-        public HrEfDb(AppNameDbContext dbContext, IMapper mapper) : base(dbContext, mapper) { }
+        public AppNameEfDb(AppNameDbContext dbContext, IMapper mapper) : base(dbContext, mapper) { }
 
         /// <summary>
         /// Gets the <see cref="Employee"/> encapsulated entity.
