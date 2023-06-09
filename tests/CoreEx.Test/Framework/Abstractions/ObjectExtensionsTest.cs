f0b950b3a2090c89cd227c3ae99a0cbc7d8ac4f5
Eric Sibly <eric.sibly@avanade.com>
2023-06-09 11:01:22 -0700

    Add ThrowIfNull extension method

---
+++ tests/CoreEx.Test/Framework/Abstractions/ObjectExtensionsTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace CoreEx.Test.Framework.Abstractions
+{
+    [TestFixture]
+    public class ObjectExtensionsTest
+    {
+        [Test]
+        public void ThrowIfNull_WhenNull()
+        {
+            string aussie = null!;
+            var ex = Assert.Throws<ArgumentNullException>(() => aussie.ThrowIfNull());
+            Assert.AreEqual("aussie", ex!.ParamName);
+        }
+
+        [Test]
+        public void ThrowIfNull_WhenNotNull()
+        {
+            string aussie = "Aussie";
+            Assert.AreEqual(aussie, aussie.ThrowIfNull());
+        }
+    }
+}
